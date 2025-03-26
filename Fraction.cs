ede9d285522bf6c96a711c8b241203bc1b5a8ac4
NazarKlyuchko <klychko2006@gmail.com>
2025-03-26 18:34:28 +0100

    Initial commit

---
+++ Fraction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FractionCalculator
+{
+    public class Fraction
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentException("Знаменник не може дорівнювати нулю.");
+
+            // Нормалізація знаку: знаменник завжди додатній
+            if (denominator < 0)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+            int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+            Numerator = numerator / gcd;
+            Denominator = denominator / gcd;
+        }
+
+        // Рекурсивний алгоритм для знаходження НСД
+        private int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+
+        public Fraction Add(Fraction other)
+        {
+            int num = this.Numerator * other.Denominator + other.Numerator * this.Denominator;
+            int den = this.Denominator * other.Denominator;
+            return new Fraction(num, den);
+        }
+
+        public Fraction Subtract(Fraction other)
+        {
+            int num = this.Numerator * other.Denominator - other.Numerator * this.Denominator;
+            int den = this.Denominator * other.Denominator;
+            return new Fraction(num, den);
+        }
+
+        public Fraction Multiply(Fraction other)
+        {
+            int num = this.Numerator * other.Numerator;
+            int den = this.Denominator * other.Denominator;
+            return new Fraction(num, den);
+        }
+
+        public Fraction Divide(Fraction other)
+        {
+            if (other.Numerator == 0)
+                throw new DivideByZeroException("Ділення на дріб з чисельником 0 недопустиме.");
+            int num = this.Numerator * other.Denominator;
+            int den = this.Denominator * other.Numerator;
+            return new Fraction(num, den);
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Fraction other)
+                return this.Numerator == other.Numerator && this.Denominator == other.Denominator;
+            return false;
+        }
+
+        public override int GetHashCode() => (Numerator, Denominator).GetHashCode();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FractionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вітаємо у калькуляторі дробів!");
+
+            try
+            {
+                // Зчитування першого дробу
+                Console.WriteLine("Введіть перший дріб (формат: чисельник/знаменник):");
+                Fraction fraction1 = ParseFraction(Console.ReadLine());
+
+                // Зчитування другого дробу
+                Console.WriteLine("Введіть другий дріб (формат: чисельник/знаменник):");
+                Fraction fraction2 = ParseFraction(Console.ReadLine());
+
+                // Обрання операції
+                Console.WriteLine("Оберіть операцію (+, -, *, /):");
+                string op = Console.ReadLine();
+
+                Fraction result = op switch
+                {
+                    "+" => fraction1.Add(fraction2),
+                    "-" => fraction1.Subtract(fraction2),
+                    "*" => fraction1.Multiply(fraction2),
+                    "/" => fraction1.Divide(fraction2),
+                    _ => throw new InvalidOperationException("Невідома операція.")
+                };
+
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+
+        static Fraction ParseFraction(string input)
+        {
+            var parts = input.Split('/');
+            if (parts.Length != 2)
+                throw new FormatException("Невірний формат дробу. Формат має бути 'чисельник/знаменник'.");
+            int numerator = int.Parse(parts[0]);
+            int denominator = int.Parse(parts[1]);
+            return new Fraction(numerator, denominator);
+        }
+    }
+}
